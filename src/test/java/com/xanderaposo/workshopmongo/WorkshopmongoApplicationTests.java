a604bb99fdcbc35bf2f7b9357e7f729a74330e7d
Xande Raposo <jorgeanraposo@hotmail.com>
2021-05-18 18:17:42 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/xanderaposo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanderaposo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
